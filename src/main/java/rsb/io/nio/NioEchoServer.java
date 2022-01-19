a8ae86d188070edd84b43793a8f8b5961e7e2456
Josh Long <josh@joshlong.com>
2022-01-19 01:12:52 -0800

    up

--- src/main/java/rsb/io/nio/NioEchoServer.java
+++ src/main/java/rsb/io/nio/NioEchoServer.java
@@ -14,54 +14,59 @@ import java.util.Locale;
 @Slf4j
 public class NioEchoServer {
 
-    public static void main(String[] args) throws Exception {
-        var selector = Selector.open();
-        var socket = ServerSocketChannel.open();
-        var serverSocket = socket.socket();
-        serverSocket.bind(new InetSocketAddress("localhost", 8089));
-        socket.configureBlocking(false);
-        socket.register(selector, SelectionKey.OP_ACCEPT, null);
-        while (true) {
-            if (selector.select() == 0)
-                continue;
-            var selectedKeys = selector.selectedKeys();
-            var keyIterator = selectedKeys.iterator();
+	public static void main(String[] args) throws Exception {
+		var selector = Selector.open();
+		var socket = ServerSocketChannel.open();
+		var serverSocket = socket.socket();
+		serverSocket.bind(new InetSocketAddress("localhost", 8089));
+		socket.configureBlocking(false);
+		socket.register(selector, SelectionKey.OP_ACCEPT, null);
+		while (true) {
+			if (selector.select() == 0)
+				continue;
+			var selectedKeys = selector.selectedKeys();
+			var keyIterator = selectedKeys.iterator();
 
-            while (keyIterator.hasNext()) {
-                var key = keyIterator.next();
+			while (keyIterator.hasNext()) {
+				var key = keyIterator.next();
 
-                if (!key.isValid())
-                    continue;
+				if (!key.isValid())
+					continue;
 
-                if (key.isAcceptable()) {
-                    var client = socket.accept();
-                    client.configureBlocking(false);
-                    client.register(selector, SelectionKey.OP_READ);
-                } //
-                else if (key.isReadable()) {
-                    var client = (SocketChannel) key.channel();
-                    var capture = ByteBuffer.allocate(1024);
-                    client.read(capture);
-                    var incoming = new String(capture.array()).trim().toUpperCase(Locale.ROOT);
-                    var reply = buildBuffer(incoming);
-                    client.register(selector, SelectionKey.OP_WRITE, reply);
-                } //
-                else if (key.isWritable()) {
-                    var client = (SocketChannel) key.channel();
-                    var buffer = (ByteBuffer) key.attachment();
-                    client.write(buffer);
-                }
-                keyIterator.remove();
-            }
-        }
-    }
+				if (key.isAcceptable()) {// connection started
+					var client = socket.accept();
+					client.configureBlocking(false);
+					client.register(selector, SelectionKey.OP_READ);
+				} //
+				else if (key.isReadable()) {// read the request
+					var client = (SocketChannel) key.channel();
+					var capture = ByteBuffer.allocate(1024);
+					client.read(capture);
+					var incoming = new String(capture.array()).trim().toUpperCase(Locale.ROOT);
+					var reply = buildBuffer(incoming);
+					client.register(selector, SelectionKey.OP_WRITE, reply); // ask to be
+																				// scheduled
+																				// to do a
+																				// write
+																				// with
+																				// reply
+				} //
+				else if (key.isWritable()) {
+					var client = (SocketChannel) key.channel();
+					var buffer = (ByteBuffer) key.attachment();
+					client.write(buffer);
+				}
+				keyIterator.remove();
+			}
+		}
+	}
 
-    private static ByteBuffer buildBuffer(String contents) {
-        var byteBuffer = ByteBuffer.allocate(1024);
-        byteBuffer.clear();
-        byteBuffer.put(contents.getBytes());
-        byteBuffer.flip();
-        return byteBuffer;
-    }
+	private static ByteBuffer buildBuffer(String contents) {
+		var byteBuffer = ByteBuffer.allocate(1024);
+		byteBuffer.clear();
+		byteBuffer.put(contents.getBytes());
+		byteBuffer.flip();
+		return byteBuffer;
+	}
 
 }
