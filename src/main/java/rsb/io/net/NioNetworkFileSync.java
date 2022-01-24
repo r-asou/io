1ebbc029bacc1350df74baf0feec74aaf5359851
Josh Long <josh@joshlong.com>
2022-01-24 01:46:59 -0800

    nio

--- src/main/java/rsb/io/net/NioNetworkFileSync.java
+++ src/main/java/rsb/io/net/NioNetworkFileSync.java
@@ -2,10 +2,8 @@ package rsb.io.net;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.FileCopyUtils;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
@@ -14,7 +12,6 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 /**
@@ -25,8 +22,6 @@ class NioNetworkFileSync implements NetworkFileSync {
 
 	public static void main(String[] args) throws Exception {
 		var nfs = new NioNetworkFileSync();
-		var file = new File("/Users/jlong/code/reactive-spring-book/io/content");
-		log.info("bytes in source file: " + FileCopyUtils.copyToByteArray(file).length);
 		nfs.start(8888, new FileSystemPersistingByteConsumer("nio"));
 	}
 
@@ -80,29 +75,22 @@ class NioNetworkFileSync implements NetworkFileSync {
 		}
 	}
 
-	private final static AtomicInteger byteBufferCount = new AtomicInteger(0);
-
 	private static void read(SelectionKey key, Selector selector, Consumer<byte[]> handler) throws Exception {
 		var ra = (ReadAttachment) key.attachment();
 		var len = 1000;
-		log.info("c: " + byteBufferCount.getAndIncrement());
 		var bb = ByteBuffer.allocate(len);
 		var channel = (SocketChannel) key.channel();
 		var read = -1;
 
-		while ((read = channel.read(bb)) >= 0) {
-			log.info("read: " + read);
+		if ((read = channel.read(bb)) >= 0) {
 			ra.buffers().add(bb);
-			bb = ByteBuffer.allocate(len);
 			channel.register(selector, SelectionKey.OP_READ, new ReadAttachment(ra.key(), ra.buffers()));
 		}
 
 		if (read == -1) {
-			log.info("< 0");
 			saveFile(ra.buffers(), handler);
 			channel.register(selector, SelectionKey.OP_WRITE);
 		}
-		log.info("buffers size: " + ra.buffers().size());
 	}
 
 	@SneakyThrows
